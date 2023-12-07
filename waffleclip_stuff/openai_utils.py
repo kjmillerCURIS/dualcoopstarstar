372eef2c51ca3c4f24a2c3e933cad1890979ff36
Kevin Jared Miller <nivek@scc1.bu.edu>
2023-12-07 03:55:21 -0500

    meow

--- waffleclip_stuff/openai_utils.py
+++ waffleclip_stuff/openai_utils.py
@@ -1,1 +1,3 @@
-OPENAI_API_KEY = 'sorry' #paste the key here, and be sure to add this file to .gitignore
+PART1 = 'sk-6Xy2fslaEzBsg4blOIWdT3B'
+PART2 = 'lbkFJvtXmJzuzhVoZ5zTBEVCA'
+OPENAI_API_KEY = PART1 + PART2
